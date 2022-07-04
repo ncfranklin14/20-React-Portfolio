21cb0ab426513b6d6ef1b0a6b439b84e3315e76d
Nicole Franklin <Ncfranklin14@gmail.com>
2022-07-04 10:10:57 -0700

    download resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { ListGroup } from 'react-bootstrap';
 import './resume.css';
-import Franklin_Resume2022 from '../../assets/Franklin_Resume2022.pdf';
+import resume from '../../assets/Resume.pdf';
 import { Button } from 'react-bootstrap';
 
 export default function Resume() {
     return (
         <section className="resume">
-            {/* <h2 className="resume-title">Experience</h2>
-            <Button className='button' variant="primary">Download Resume</Button>{' '}
-            <aside className='techSkills'>
-                <h3>Technical Skills:</h3>
-            </aside> */}
+            <h2 className="resume-title">Experience</h2>
+            <div className='linkToRes'>
+            <a href={resume} download="Resume.pdf">Download Resume </a>
+            </div>
             <div className='resumeContainer'>
                 <ListGroup className='skills'>
                     <div className='front'>
@@ -21,13 +20,13 @@ export default function Resume() {
                         <ListGroup.Item>HTML5</ListGroup.Item>
                         <ListGroup.Item>JQuery </ListGroup.Item>
                         <ListGroup.Item>Bootstrap </ListGroup.Item>
+                        <ListGroup.Item>Handlebars</ListGroup.Item>
                         <ListGroup.Item>ReactJS</ListGroup.Item>
                     </div>
                     <div className='back'>
                         <h4>Back-end:</h4>
                         <ListGroup.Item>NodeJS</ListGroup.Item>
                         <ListGroup.Item>Express</ListGroup.Item>
-                        <ListGroup.Item>Handlebars</ListGroup.Item>
                         <ListGroup.Item>MySQL/Sequelize </ListGroup.Item>
                         <ListGroup.Item>MongoDB/Mongoose</ListGroup.Item>
                         <ListGroup.Item>GitHub</ListGroup.Item>
