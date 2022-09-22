9a386f175773deb1cc30323753be02664f8a2283
Nicole Franklin <Ncfranklin14@gmail.com>
2022-09-22 14:36:15 -0700

    icon updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.css';
-import 'bulma/css/bulma.min.css';
+// import 'bulma/css/bulma.min.css';
 // import Header from './Header';
 // import Footer from './Footer';
 import PortfolioContainer from "./components/PortfolioContainer";
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,17 +5,11 @@ import emailIcon from '../assets/email-icon.png'
 
 const Footer = () => (
   <footer className="footer">
-    <div className='content has-text-centered'>
-                        <div className='icon is-large fas fa-2x'>
-                          <div className='contactIcon'>
-                            <a href="https://github.com/ncfranklin14" target="blank"><img src={github} alt="Github" id="links" className='git'/></a>
-
-                            <a href="https://www.linkedin.com/in/nicole-franklin-8a6001a1/" target="blank"><img src={linkedIn} alt="Linkedin" id="links" className='linkedIn'/></a>
-
-                            <a href="mailto: ncfranklin14@gmail.com" target="blank"><img src={emailIcon} alt="email" id="links" className='email'/></a>
-                          </div>
-                        </div>
-        </div>
+            <address>
+                <a href="mailto:ncfranklin14@gmail.com"><img alt="email logo" src={emailIcon} width='50'/></a>
+                <a href="https://github.com/ncfranklin14" target="_blank"><img alt="Github logo" src={github} width='50'/></a>
+                <a href="https://www.linkedin.com/in/nicole-franklin-8a6001a1/" target="_blank"><img alt="LinkedIn logo" src={linkedIn} width='50'/></a>
+            </address>
   </footer>
 );
 
