a10280c9a07725a2423553d3bc57534f0aaa3364
Nicole Franklin <Ncfranklin14@gmail.com>
2022-10-26 09:15:49 -0700

    button and skills styling

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -2,38 +2,17 @@ import React from 'react';
 import { ListGroup } from 'react-bootstrap';
 import './resume.css';
 import resume from '../../assets/Resume.pdf';
-import { Button } from 'react-bootstrap';
 
 export default function Resume() {
     return (
         <section className="resume">
             <h2 className="resume-title">Experience</h2>
-            <div className='linkToRes'>
-                <a href={resume} download="Resume.pdf">Download Resume </a>
-            </div>
             <div className='resumeContainer'>
-                <div className='front'>
-                    <h4>Front-end:</h4>
-                    <ul>
-                        <li>JavaScript</li>
-                        <li>CSS</li>
-                        <li>HTML5</li>
-                        <li>JQuery</li>
-                        <li>Bootstrap</li>
-                        <li>Handlebars</li>
-                        <li>ReactJS</li>
-                    </ul>
-                </div>
-                <div className='back'>
-                    <h4>Back-end:</h4>
-                    <ul>
-                        <li>NodeJS</li>
-                        <li>Express</li>
-                        <li>MySQL/Sequelize </li>
-                        <li>MongoDB/Mongoose</li>
-                    </ul>
+                <div className='skills'>
+                    JavaScript || CSS || HTML5 || JQuery || Bootstrap || Handlebars || ReactJS || NodeJS || Express || MySQL/Sequelize || MongoDB/Mongoose
                 </div>
             </div>
+            <a className='linkToRes' href={resume} download="Resume.pdf">Download Resume </a>
         </section >
     );
 }
